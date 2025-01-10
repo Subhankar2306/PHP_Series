ddf3935e88acab062a71d1708417a7c44e34a299
Subhankar <subhankardhal637@gmail.com>
2025-01-10 17:49:17 +0530

    Showing data in tabular formate

--- searchEmp.php
+++ searchEmp.php
@@ -15,13 +15,24 @@
       $n=mysqli_num_rows($record);  // This method is use to count the record present in the table.
       // echo "<br>Total Record:".$n;
       if($n>0){
-        while($row=mysqli_fetch_row($record)){
-          echo $row[0]." ".$row[1]." ".$row[2]." ".$row[3]." ";
-          echo "<br>"; 
+        echo "<table border = 1>";
+        echo "<tr><th>Emp_Id</th><th>Emp_Name</th><th>Salary</th><th>Departmrnt</th></tr>";
+        while($row=mysqli_fetch_assoc($record)){
+          // echo $row[0]." ".$row[1]." ".$row[2]." ".$row[3]." ";
+          echo "<tr>";
+          echo "<td>".$row['emp_id']."</td>";
+          echo "<td>".$row['emp_name'] ."</td>";
+          echo "<td>".$row['salary'] ."</td>";
+          echo "<td>".$row['department'] ."</td>";
+          // echo $row['emp_id']." ".$row['emp_name']." ".$row['salary']." ".$row['department']." ";   // When we use mysqli_fetch_assoc then we have to write record name instade of index.
+          // echo "<br>";
+          echo "</tr>" ;
         }
+        echo "</table>";
       } 
       else
       echo "<font color=red size=5>Record not found</font>";
+    $mycon->close();
     ?>
   </center>
 </body>
