55dee86fcb93987ce60fbbaf47393e2188c12153
Subhankar <subhankardhal637@gmail.com>
2025-01-10 15:42:13 +0530

    Taking input from user

--- insertEmp.php
+++ insertEmp.php
@@ -7,21 +7,46 @@
 </head>
 <body>
   <center>
-    <?php
-    $empid = 100;
-    $empname = "Subhankar Dhal";
-    $empsal = 10000;
-    $edepat = 'IT';
+    <form method = post action="" >
+      <table border = "1">
+        <tr>
+          <th>Id: </th>
+          <td><input type="text" name= txtId></td>
+        </tr>
+        <tr>
+          <th>Name: </th>
+          <td><input type="text" name= txtName></td>
+        </tr>
+        <tr>
+          <th>Salary: </th>
+          <td><input type="text" name= txtSalary></td>
+        </tr>
+        <tr>
+          <th>Department: </th>
+          <td><input type="text" name =txtDept></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="submit" value="Submit" name = "btnsubmit"></td>
+        </tr>
+      </table>
+    </form>
 
-    // 1. Connection
-    $myconnection = mysqli_connect("localhost", "root", "", "mydata");
-    echo " Connection Successfully"; 
-    // 2. Query
-    $sql = "insert into employee values(?,?,?,?)";
-    $ps = $myconnection->prepare($sql);
-    $ps->bind_param("isis", $empid, $empname, $empsal, $edepat);
-    $ps->execute();
-    echo "<br> Record inserted Successfully.";  // This way we can connect to the database, and inserted data to ta table.
+    <?php
+    if(isset($_POST['btnsubmit']))
+    {
+    $empid = $_POST['txtId'];
+    $empname = $_POST['txtName'];
+    $empsal = $_POST['txtSalary'];
+    $edepat = $_POST['txtDept'];
+      $myconnection = mysqli_connect("localhost", "root", "", "mydata");
+      echo " Connection Successfully"; 
+      $sql = "insert into employee values(?,?,?,?)";
+      $ps = $myconnection->prepare($sql);
+      $ps->bind_param("isis", $empid, $empname, $empsal, $edepat);
+      $ps->execute();
+      echo "<br> Record inserted Successfully.";  // This way we can connect to the database, and inserted data to ta table.
+    }
     ?>
   </center>
 </body>
